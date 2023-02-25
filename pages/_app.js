5815fc1c085d9e0c9853e3d414108fde6a309a56
David Chedrick <davidchedrick@gmail.com>
2023-02-24 23:15:32 -0600

    added jest

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Page from "@/components/Page";
+import TestPage from "@/components/cat";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
 	return (
 		<Page>
 			<Component {...pageProps} />
+			<TestPage />
 		</Page>
 	);
 }
